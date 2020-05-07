b0dd95d5c07d3773ae042e11a42e72ba84f85fff
Jan Silva <janfrs3@gmail.com>
2020-05-07 14:23:01 -0300

    Vertx Ws: Use Coroutines with vertx dispatcher

--- vertex-ws/src/main/kotlin/org/jan/controller/UserAccountController.kt
+++ vertex-ws/src/main/kotlin/org/jan/controller/UserAccountController.kt
@@ -6,36 +6,44 @@ import io.vertx.core.eventbus.Message
 import io.vertx.core.json.Json
 import io.vertx.core.json.JsonObject
 import io.vertx.ext.web.RoutingContext
+import io.vertx.kotlin.core.eventbus.requestAwait
+import io.vertx.kotlin.core.json.jsonObjectOf
+import io.vertx.kotlin.coroutines.dispatcher
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import org.jan.repository.UserAccountRepositoryVerticle
 
 object UserAccountController {
 
     val insertUser = Handler<RoutingContext> { routingContext ->
+        GlobalScope.launch(routingContext.vertx().dispatcher()) {
+            val response = routingContext.response()
+                    .putHeader("content-type", "application/json")
+                    .setChunked(true)
 
-        val response = routingContext.response()
-            .putHeader("content-type", "application/json")
-            .setChunked(true)
+            try {
+                val userAccount: Message<JsonObject> = routingContext.vertx().eventBus().requestAwait(
+                        UserAccountRepositoryVerticle.USER_ACCOUNT_REPOSITORY_INSERT,
+                        routingContext.bodyAsJson
+                )
 
-
-        routingContext.vertx().eventBus().request(
-            UserAccountRepositoryVerticle.USER_ACCOUNT_REPOSITORY_INSERT,
-            routingContext.bodyAsJson,
-            Handler<AsyncResult<Message<JsonObject>>> { asyncRes ->
-                if (asyncRes.succeeded()) {
-                    val userAccount = asyncRes.result().body()
-                    response
+                println(Thread.currentThread().getName())
+                response
                         .setStatusCode(201)
                         .setStatusMessage("Created")
-                        .write(Json.encodePrettily(userAccount))
+                        .write(Json.encodePrettily(userAccount.body()))
                         .end()
-                } else {
-                    response
+            } catch(e: Exception) {
+                response
                         .setStatusCode(400)
                         .setStatusMessage("Bad Request")
-                        .write(asyncRes.toString())
+                        .write(
+                                jsonObjectOf(
+                                    "message" to e.message
+                                ).toBuffer()
+                        )
                         .end()
-                }
             }
-        )
+        }
     }
 }
--- vertex-ws/src/main/kotlin/org/jan/repository/UserAccountRepositoryVerticle.kt
+++ vertex-ws/src/main/kotlin/org/jan/repository/UserAccountRepositoryVerticle.kt
@@ -1,67 +1,60 @@
 package org.jan.repository
 
 import io.vertx.core.Handler
+import io.vertx.core.Vertx
 import io.vertx.core.eventbus.Message
 import io.vertx.core.json.Json
 import io.vertx.core.json.JsonObject
 import io.vertx.kotlin.coroutines.CoroutineVerticle
+import io.vertx.kotlin.coroutines.dispatcher
 import io.vertx.kotlin.pgclient.pgConnectOptionsOf
+import io.vertx.kotlin.pgclient.preparedQueryAwait
 import io.vertx.kotlin.sqlclient.poolOptionsOf
 import io.vertx.pgclient.PgPool
 import io.vertx.sqlclient.Tuple
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 
 const val ERROR_ON_SAVING_USER_ACCOUNT = 1
 
 class UserAccountRepositoryVerticle : CoroutineVerticle() {
 
+    private val connectOptions = pgConnectOptionsOf(
+            port = 5432,
+            host = "172.19.0.2",
+            database = "dev_db",
+            user = "postgres",
+            password = "postgres"
+    )
+    // Pool options
+
+    // Pool options
+    private val poolOptions = poolOptionsOf()
+            .setMaxSize(5)
+
+    private fun getConn(vertx: Vertx) : PgPool = PgPool.pool(vertx, connectOptions, poolOptions)
+
     override suspend fun start() {
         vertx.eventBus().consumer(USER_ACCOUNT_REPOSITORY_INSERT,createUserAccount)
     }
 
     private val createUserAccount = Handler { message: Message<JsonObject> ->
+        GlobalScope.launch(vertx.dispatcher()) {
+            val userAccount: JsonObject = message.body()
 
-        //save to postgres database
-        // Connect options
-        // Connect options
-        val userAccount: JsonObject = message.body()
-
-        val connectOptions = pgConnectOptionsOf(
-            port = 5432,
-            host = "172.22.0.2",
-            database = "dev_db",
-            user = "postgres",
-            password = "postgres"
-        )
-        // Pool options
-
-        // Pool options
-        val poolOptions = poolOptionsOf()
-            .setMaxSize(5)
-
-        // Create the client pool
-        val client: PgPool = PgPool.pool(vertx, connectOptions, poolOptions)
-
-        // A simple query
-        client.preparedQuery(
-            "INSERT INTO user_account (name, password, email) VALUES ($1, $2, $3)",
-            Tuple.of(
-                userAccount.getString("name"),
-                userAccount.getString("password"),
-                userAccount.getString("email")
-            )
-        ) { ar ->
-            run {
-                if (ar.succeeded()) {
-                    message.reply(userAccount)
-                } else {
-                    message.fail(
-                        ERROR_ON_SAVING_USER_ACCOUNT,
-                        "message: " + ar.cause().toString()
-                    )
-                }
-                // Now close the pool
-                client.close();
+            try {
+                getConn(vertx).preparedQueryAwait(
+                        "INSERT INTO user_account (name, password, email) VALUES ($1, $2, $3)",
+                        Tuple.of(
+                                userAccount.getString("name"),
+                                userAccount.getString("password"),
+                                userAccount.getString("email")
+                        )
+                )
+                message.reply(userAccount)
+            } catch (e: Exception) {
+                message.fail(ERROR_ON_SAVING_USER_ACCOUNT, "message: " + e.message)
             }
         }
     }
