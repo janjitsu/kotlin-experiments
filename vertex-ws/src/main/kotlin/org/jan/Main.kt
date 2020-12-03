22c2de714f42b8dee0413efeca2474de24f80f3e
Jan Silva <janfrs3@gmail.com>
2020-12-03 10:12:51 -0300

    Add Jeager OpenTracing

--- vertex-ws/src/main/kotlin/org/jan/Main.kt
+++ vertex-ws/src/main/kotlin/org/jan/Main.kt
@@ -2,16 +2,18 @@ package org.jan
 
 import io.vertx.config.ConfigRetriever
 import io.vertx.core.Vertx
+import io.vertx.core.VertxOptions
 import io.vertx.kotlin.config.configRetrieverOptionsOf
 import io.vertx.kotlin.config.configStoreOptionsOf
 import io.vertx.kotlin.core.deploymentOptionsOf
 import io.vertx.kotlin.core.json.jsonObjectOf
 import io.vertx.kotlin.coroutines.await
+import io.vertx.tracing.opentracing.OpenTracingOptions
 import org.jan.repository.UserAccountRepositoryVerticle
 
 
 suspend fun main() {
-    val vertx = Vertx.vertx()
+    val vertx = Vertx.vertx(VertxOptions().setTracingOptions(OpenTracingOptions()))
     val retriever = ConfigRetriever.create(
         vertx,
         configRetrieverOptionsOf(
